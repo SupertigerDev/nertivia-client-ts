c0c6648e194a1ca86be5760ae2ac14d007b5e0c0
supertiger1234 <saawalkhn@googlemail.com>
2020-10-11 20:40:27 +0100

    show delete & edit button only if the msg is yours

--- src/components/chat-area/message/MessageContextMenu.vue
+++ src/components/chat-area/message/MessageContextMenu.vue
@@ -6,27 +6,43 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 import ContextMenu from "@/components/ContextMenu.vue";
 import Message from "@/interfaces/Message";
+import { MeModule } from "@/store/modules/me";
 
 @Component({ components: { ContextMenu } })
 export default class extends Vue {
   @Prop() private message!: Message & { grouped: boolean };
   @Prop() private pos!: { x?: number; y?: number };
   get items() {
-    return [
+    const items: any = [
       {
         name: "Copy",
         icon: "developer_board"
-      },
-      {
-        name: "Edit",
-        icon: "edit"
-      },
-      { type: "seperator" },
-      {
-        name: "Delete",
-        icon: "delete"
       }
     ];
+    if (this.messageCreatedByMe) {
+      items.push({
+        name: "Edit",
+        icon: "edit"
+      });
+    }
+    if (this.canDeleteMessage) {
+      items.push(
+        { type: "seperator" },
+        {
+          name: "Delete",
+          icon: "delete"
+        }
+      );
+    }
+
+    return items;
+  }
+  get messageCreatedByMe() {
+    return MeModule.user.uniqueID === this.message.creator.uniqueID;
+  }
+  get canDeleteMessage() {
+    if (this.messageCreatedByMe) return true;
+    return false;
   }
 }
 </script>
