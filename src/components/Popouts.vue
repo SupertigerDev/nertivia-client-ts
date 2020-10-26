62db405c14dc7bfccb9aa0345dbb99b06e447084
supertiger1234 <saawalkhn@googlemail.com>
2020-10-26 10:49:10 +0000

    fixed bug

--- src/components/Popouts.vue
+++ src/components/Popouts.vue
@@ -24,3 +24,15 @@ export default class MainApp extends Vue {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.popouts {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  pointer-events: none;
+  z-index: 999999999999;
+}
+</style>
