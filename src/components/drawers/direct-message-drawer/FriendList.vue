fa0aede827bfc3ec1623969359518307fa49e07d
supertiger1234 <saawalkhn@googlemail.com>
2020-12-31 22:00:01 +0000

    added request template

--- src/components/drawers/direct-message-drawer/FriendList.vue
+++ src/components/drawers/direct-message-drawer/FriendList.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="friend-list">
     <virtual-list :size="260" :remain="40" :variable="true">
+      <div class="tab" :style="{ height: '25px' }">Requests</div>
+      <FriendRequestTemplate
+        v-for="friend in friendRequests"
+        :style="{ height: '79px' }"
+        :key="friend.recipient.uniqueID"
+        :friend="friend"
+      />
       <div class="tab" :style="{ height: '25px' }">Online</div>
       <FriendTemplate
         v-for="friend in onlineFriends"
@@ -23,10 +30,11 @@
 import { FriendsModule } from "@/store/modules/friends";
 import { Component, Vue } from "vue-property-decorator";
 import FriendTemplate from "./FriendTemplate.vue";
+import FriendRequestTemplate from "./FriendRequestTemplate.vue";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const virtualList = require("vue-virtual-scroll-list");
 
-@Component({ components: { FriendTemplate, virtualList } })
+@Component({ components: { FriendTemplate, FriendRequestTemplate, virtualList } })
 export default class FriendList extends Vue {
   get friends() {
     return FriendsModule.friendsWithUser;
@@ -36,6 +44,9 @@ export default class FriendList extends Vue {
   }
   get offlineFriends() {
     return this.friends.filter(f => !f.presence && f.status === 2);
+  }
+  get friendRequests() {
+    return this.friends.filter(f => f.status <= 1);
   }
 }
 </script>
---
+++ src/components/drawers/direct-message-drawer/FriendRequestTemplate.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="friend">
+    <div
+      class="wrapper"
+      @click="clickedEvent"
+      @mouseover="hover = true"
+      @mouseleave="hover = false"
+    >
+    <div class="inner-content">
+      <AvatarImage
+        class="avatar"
+        :imageId="user.avatar"
+        :seedId="user.uniqueID"
+        size="30px"
+        :willHaveClickEvent="true"
+        :animateGif="hover"
+        @click.native="showProfile"
+      />
+      <div class="details">
+          <div class="username">{{ user.username }}</div>
+          <div class="status">
+            <div class="name">{{ friend.status === 1  ? "Incoming" : 'Request Sent'}}</div>
+          </div>
+      </div>  
+    </div>  
+      <div class="buttons">
+        <div class="button alert" v-if="friend.status === 0">Cancel</div>
+        <div class="button" v-if="friend.status === 1">Accept</div>
+        <div class="button alert" v-if="friend.status === 1">Decline</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import AvatarImage from "@/components/AvatarImage.vue";
+import User from "@/interfaces/User";
+import { ChannelsModule } from "@/store/modules/channels";
+import { PopoutsModule } from "@/store/modules/popouts";
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({ components: { AvatarImage } })
+export default class FriendTemplate extends Vue {
+  @Prop() private friend!: { recipient: User; status: number };
+  @Prop() private dmChannel!: { recipients: User[] };
+  hover = false;
+  clickedEvent(event: any) {
+    if (!event.target.closest(".avatar")) {
+      ChannelsModule.LoadDmChannel(this.user.uniqueID);
+    }
+  }
+  showProfile() {
+    PopoutsModule.ShowPopout({
+      id: "profile",
+      component: "profile-popout",
+      data: { uniqueID: this.user.uniqueID }
+    });
+  }
+  get user() {
+    if (this.friend) {
+      return this.friend.recipient || this.friend;
+    } else {
+      return this.dmChannel.recipients[0];
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  height: 80px;
+  flex-shrink: 0;
+  cursor: pointer;
+  user-select: none;
+  margin: 5px;
+  margin-top: 2px;
+  margin-bottom: 2px;
+  transition: 0.2s;
+}
+.inner-content {
+  display: flex;
+  flex-shrink: 0;
+  align-content: center;
+  align-items: center;
+  margin-top: 5px;
+}
+.username {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.avatar {
+  margin-right: 5px;
+  margin-left: 5px;
+}
+.status {
+  display: flex;
+  align-items: center;
+  align-content: center;
+  font-size: 14px;
+  .name {
+    opacity: 0.8;
+  }
+}
+.buttons {
+  display: flex;
+  margin-top: 5px;
+  flex-shrink: 0;
+}
+.button {
+  flex: 1;
+  border-radius: 4px;
+  padding: 5px;
+  text-align: center;
+  margin-left: 5px;
+  margin-right: 5px;
+  opacity: 0.7;
+  transition: 0.2s;
+  background: rgba(255, 255, 255, 0.1);
+  &:hover {
+    background: var(--primary-color);
+    opacity: 1;
+  }
+  &.alert:hover {
+    background: var(--alert-color);
+  }
+}
+</style>
