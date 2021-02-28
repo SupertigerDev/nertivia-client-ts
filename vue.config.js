bcd4aea8010d2c6d3374d30f9a8f4d831be85318
Supertiger <saawalkhn@googlemail.com>
2021-02-28 13:58:49 +0000

    added vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transpileDependencies: ["vuex-module-decorators"],
+  configureWebpack: {},
+  pwa: {
+    manifestOptions: {
+      name: "Nertivia BETA",
+      short_name: "Nertivia BETA",
+      start_url: "/app"
+    },
+    themeColor: "#181c22",
+    backgroundColor: "#222831"
+  }
+};
