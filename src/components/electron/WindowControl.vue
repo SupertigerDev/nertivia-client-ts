99e198255e71fe09abc3cac641b7410570366aeb
Supertiger <saawalkhn@googlemail.com>
2021-03-06 09:26:01 +0000

    make window draggable from the top (electron)

--- src/components/electron/WindowControl.vue
+++ src/components/electron/WindowControl.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="window-controls">
-    <div class="title">Nertivia</div>
+    <div class="drag-area">
+      <div class="title">Nertivia</div>
+    </div>
   </div>
 </template>
 
@@ -15,16 +17,24 @@ export default class App extends Vue {}
 .window-controls {
   display: flex;
   flex-shrink: 0;
-  align-items: center;
   width: 100%;
-  height: 30px; 
+  height: 30px;
   background: rgba(0, 0, 0, 0.4);
+  overflow: hidden;
+}
+.drag-area {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 100%;
   -webkit-user-select: none;
   -webkit-app-region: drag;
-  border-bottom-right-radius: 4px;
-  border-bottom-left-radius: 4px;
+  margin-top: 3px;
+  margin-left: 3px;
+  margin-right: 3px;
 }
 .title {
   margin-left: 5px;
+  margin-bottom: 3px;
 }
 </style>
