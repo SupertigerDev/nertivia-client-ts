9c25bcb3bb648e7f5bff00cee7895fb4cb781e30
Supertiger <saawalkhn@googlemail.com>
2021-03-07 19:50:52 +0000

    removed console log

--- src/store/modules/channels.ts
+++ src/store/modules/channels.ts
@@ -45,7 +45,6 @@ class Channels extends VuexModule {
       const server = ServersModule.servers[id];
       const channel_position = server.channel_position;
       if (channel_position && channel_position.length) {
-        console.log(this.serverChannels(id))
         return this.serverChannels(id).sort((a, b) => {
           const aIndex = channel_position.indexOf(a.channelID);
           const bIndex = channel_position.indexOf(b.channelID);
