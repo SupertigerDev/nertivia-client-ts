a0c0032815e44e6cea85dd7eea9bb306467f9692
Supertiger <saawalkhn@googlemail.com>
2021-03-07 15:45:04 +0000

    added window control buttons

--- src/components/electron/WindowControl.vue
+++ src/components/electron/WindowControl.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="window-controls">
     <div class="drag-area">
+      <img class="logo" src="@/assets/logo.svg" />
       <div class="title">Nertivia</div>
+    </div>
+    <div class="buttons">
+      <div class="button material-icons" @click="minimize">minimize</div>
+      <div class="button material-icons" @click="maximize">stop</div>
+      <div class="button material-icons warn" @click="close">close</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import electronBridge from "@/utils/electronBridge";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  minimize() {
+    electronBridge.send("window_action", "minimize");
+  }
+  maximize() {
+    electronBridge.send("window_action", "maximize");
+  }
+  close() {
+    electronBridge.send("window_action", "close");
+  }
+}
 </script>
 
 <style lang="scss">
@@ -26,7 +43,7 @@ export default class App extends Vue {}
   display: flex;
   align-items: center;
   height: 100%;
-  width: 100%;
+  flex: 1;
   -webkit-user-select: none;
   -webkit-app-region: drag;
   margin-top: 3px;
@@ -37,5 +54,39 @@ export default class App extends Vue {}
   margin-left: 5px;
   margin-bottom: 3px;
   color: rgba(255, 255, 255, 0.6);
+}
+.buttons {
+  display: flex;
+  overflow: hidden;
+  align-self: center;
+  margin-right: 3px;
+}
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  width: 50px;
+  height: 23px;
+  margin-left: 5px;
+  border-radius: 4px;
+  transition: 0.2s;
+  user-select: none;
+  margin-bottom: 1px;
+  background: rgba(255, 255, 255, 0.1);
+  color: rgba(255, 255, 255, 0.7);
+  &:hover {
+    color: white;
+    background: var(--primary-color);
+    &.warn {
+      background: var(--alert-color);
+    }
+  }
+}
+.logo {
+  height: 25px;
+  width: 25px;
+  margin-bottom: 4px;
+  margin-left: 2px;
 }
 </style>
