0e2e6572e9b7d857be1c4c8546d000e5a6ca5339
Supertiger <saawalkhn@googlemail.com>
2021-03-17 11:05:14 +0000

    electron app badge

--- src/interfaces/ElectronBridge.ts
+++ src/interfaces/ElectronBridge.ts
@@ -1,4 +1,4 @@
-type EventTypes = "window_action";
+type EventTypes = "window_action" | "notification_badge";
 export default interface ElectronBridge {
   isElectron: boolean;
   send: (event: EventTypes, data?: any) => void;
