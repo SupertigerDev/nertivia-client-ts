0f7df2a794960d7bdafbac173f154d2b566acc06
Supertiger <saawalkhn@googlemail.com>
2021-04-12 18:09:27 +0100

    changed timeout to 60 seconds

--- src/services/wrapper.ts
+++ src/services/wrapper.ts
@@ -4,6 +4,7 @@ function createInstance() {
   return ky.create({
     prefixUrl: process.env.VUE_APP_FETCH_PREFIX,
     retry: 0,
+    timeout: 60000,
     headers: {
       authorization: localStorage.getItem("hauthid") || "",
     },
