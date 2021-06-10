2da21335c0a184f633abec27e131d740ddfb37a0
Supertiger <saawalkhn@googlemail.com>
2021-06-10 10:22:26 +0100

    markupo inside quotes

--- src/components/markup/MessageQuote.vue
+++ src/components/markup/MessageQuote.vue
@@ -1,84 +1,103 @@
-<template>
-  <div class="quote" v-bind:class="{ quoteFailed: failMessage }">
-    <AvatarImage
-      :seedId="this.quote.creator.id"
-      :imageId="this.quote.creator.avatar"
-      :willHaveClickEvent="true"
-      @click.native="showProfile"
-      size="40px"
-    />
-    <div class="content">
-      <div class="details">
-        <div
-          class="username"
-          @click="showProfile"
-          v-text="this.quote.creator.username"
-        />
-        <div class="badge bot" v-if="this.quote.creator.bot">BOT</div>
-        <!-- <div class="date">DATE</div> -->
-      </div>
-      <div class="message">{{ failMessage || this.quote.message }}</div>
-    </div>
-    <div
-      class="material-icons goto-message-icon"
-      title="Go To Message"
-      @click="goToMessage"
-    >
-      keyboard_arrow_up
-    </div>
-  </div>
-</template>
-
 <script lang="tsx">
 import AvatarImage from "@/components/AvatarImage.vue";
 import type { Quote } from "@/interfaces/Message";
-import { Component, Prop, Vue } from "vue-property-decorator";
 import { PopoutsModule } from "@/store/modules/popouts";
 import User from "@/interfaces/User";
 import { ServerMembersModule } from "@/store/modules/serverMembers";
 import Markup from "@/components/Markup";
 import { eventBus } from "@/utils/globalBus";
+import Vue from "vue";
 
-@Component({components: {AvatarImage, Markup}})
-export default class MessageQuote extends Vue {
-  @Prop() private quote!: Quote;
-  @Prop() private user?: User;
-
-  get failMessage() {
-    if (!this.quote?.message) {
-      return "File Message";
-    }
-    if (this.quote?.message?.length > 1000) {
-      return "This quote is too long to be displayed";
-    } else {
-      return null;
-    }
-  }
-  goToMessage() {
-    eventBus.$emit('scrollToMessage', this.quote.messageID)
-  }
-
-  showProfile(event: PointerEvent) {
-    if (this.user != null) {
-      PopoutsModule.ShowPopout({
-        id: "profile",
-        component: "MiniProfilePopout",
-        data: {
-          x: event.x,
-          y: event.y,
-          member: {
-            member: this.user,
-            roles:
-              ServerMembersModule.memberRoles(
-                this.$route.params.server_id,
-                this.user.id
-              ) ?? [],
-          },
+export default Vue.extend({
+  components: { AvatarImage, Markup },
+  props: {
+    quote: Object,
+    user: Object,
+  },
+  render(h) {
+    const AvatarBuilder = (creator: any) => {
+      return h(AvatarImage, {
+        nativeOn: {
+          click: this.showProfile
+        },
+        props: {
+          seedId: creator.id,
+          imageId: creator.avatar,
+          willHaveClickEvent: true,
+          size: "40px",
         },
       });
-    }
-  }
-}
+    };
+
+    const GotoMessageBuilder = (
+      <div
+        class="material-icons goto-message-icon"
+        title="Go To Message"
+        onClick={this.gotoMessage}
+      >
+        keyboard_arrow_up
+      </div>
+    );
+    const MarkupBuilder = this.quote.message && h(Markup, {props: {text: this.quote.message}})
+
+    const ContentBuilder = (
+      <div class="content">
+        <div class="details">
+          <div
+            class="username"
+            onClick={this.showProfile}
+          >{this.quote.creator.username}</div>
+        </div>
+        <div class="message">{this.failMessage || MarkupBuilder}</div>
+      </div>
+    );
+
+    return (
+      <div class="quote">
+        {AvatarBuilder(this.quote.creator)}
+        {ContentBuilder}
+        {GotoMessageBuilder}
+      </div>
+    );
+  },
+  methods: {
+    gotoMessage() {
+      eventBus.$emit("scrollToMessage", this.quote.messageID);
+    },
+    showProfile(event: PointerEvent) {
+      if (this.user != null) {
+        PopoutsModule.ShowPopout({
+          id: "profile",
+          component: "MiniProfilePopout",
+          data: {
+            x: event.x,
+            y: event.y,
+            member: {
+              member: this.user,
+              roles:
+                ServerMembersModule.memberRoles(
+                  this.$route.params.server_id,
+                  this.user.id
+                ) ?? [],
+            },
+          },
+        });
+      }
+    },
+  },
+  computed: {
+    failMessage() {
+      if (!this.quote?.message) {
+        return "File Message";
+      }
+      if (this.quote?.message?.length > 1000) {
+        return "This quote is too long to be displayed";
+      } else {
+        return null;
+      }
+    },
+  },
+});
 </script>
 
 <style lang="scss" scoped>
