837b8be0a8329225dace7e7faa8b45fa24668504
brecert <me@bree.dev>
2021-06-18 05:01:38 -0400

    fix unzip function to base64 decode values again

--- src/utils/zip.ts
+++ src/utils/zip.ts
@@ -1,23 +1,19 @@
 import pako from "pako";
+import * as Base64 from "base64-arraybuffer";
 
 export function zip(value: string) {
   const binaryString = pako.deflate(value, { to: "string" });
   return btoa(binaryString);
 }
 
+const textDecoder = new TextDecoder();
+
 export function unzip(base64: string) {
   try {
-    const binaryString = atob(base64);
-    return pako.inflate(binaryString, { to: "string" });
-  } catch {
-    return null;
-  }
-}
-
-export function unzipAlt(base64: string) {
-  try {
-    return pako.inflate(base64, { to: "string" });
-  } catch {
+    const base64buf: ArrayBuffer = Base64.decode(base64);
+    return pako.inflate(textDecoder.decode(base64buf), { to: "string" });
+  } catch (err) {
+    console.warn(err);
     return null;
   }
 }
