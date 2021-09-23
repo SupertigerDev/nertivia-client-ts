2f0cbfd1eb5e68ac797ac7236e7cdc2f82b76337
Supertiger <saawalkhn@googlemail.com>
2021-09-23 10:59:57 +0100

    uh, what else to fix? i think im done

--- src/components/chat-area/message/HTMLEmbed.vue
+++ src/components/chat-area/message/HTMLEmbed.vue
@@ -1,7 +1,7 @@
 <script lang="tsx">
-import Vue from "vue";
+import Vue, { h } from "vue";
 import { unzipAlt } from "@/utils/zip";
-import { JsonInput } from "jsonhtmlfyer";
+import { JsonInput, jsonToHtml } from "jsonhtmlfyer";
 import Markup from "@/components/Markup";
 import { PopoutsModule } from "@/store/modules/popouts";
 
@@ -9,86 +9,88 @@ interface Props {
   compressedJSON: string;
 }
 import { defineComponent } from "vue";
-export default defineComponent<Props>({
-  functional: true,
-  render(h, { props }) {
-    let obj: any = {};
-    try {
-      obj = JSON.parse(unzipAlt(props.compressedJSON) || "");
-    } catch {
-      return <div />;
-    }
-    const clickEvent = (event: any) => {
-      event.preventDefault();
-      const target = event.target.closest("a");
-      if (!target) return;
-      const url = target.getAttribute("href");
-      if (!url) return;
-
-      PopoutsModule.ShowPopout({
-        id: "html-embed-url-sus",
-        component: "OpenLinkConfirm",
-        data: { url }
-      });
-    };
-
-    const template = (json: JsonInput) => {
-      if (json.attributes?.href) {
-        if (!json.attributes.href.startsWith("http")) {
-          json.attributes.href = "https://" + json.attributes.href;
-        }
-      }
-      if (json.styles?.backgroundImage) {
-        json.styles.backgroundImage = `url(${
-          process.env.VUE_APP_IMAGE_PROXY_URL
-        }${encodeURIComponent(json.styles.backgroundImage)})`;
-      }
-      if (json.tag === "img" && json.attributes?.src) {
-        json.attributes.src =
-          process.env.VUE_APP_IMAGE_PROXY_URL +
-          encodeURIComponent(json.attributes.src);
-      }
-      if (json.styles?.position === "fixed") {
-        delete json.styles.position;
-      }
-      if (json.styles?.background) {
-        delete json.styles.background;
-      }
-      const el = h(json.tag, {
-        style: json.styles,
-        attrs: json.attributes
-      });
-      el.children = [];
-      if (typeof json.content === "object") {
-        if (Array.isArray(json.content)) {
-          for (let i = 0; i < json.content.length; i++) {
-            el.children.push(template(json.content[i]));
-          }
-          return el;
-        } else {
-          el.children.push(template(json.content));
-          return el;
-        }
-      }
-      if (json.content) {
-        el.children = [
-          <div class="content">
-            {h(Markup, {
-              props: {
-                text: json.content
-              }
-            })}
-          </div>
-        ];
-      }
-      return el;
-    };
-
-    return h("div", { class: "html-embed", on: { click: clickEvent } }, [
-      template(obj)
-    ]);
+export default function HTMLEmbed(props) {
+  let obj: any = {};
+  try {
+    obj = JSON.parse(unzipAlt(props.compressedJSON) || "");
+  } catch {
+    return <div />;
   }
-});
+  const clickEvent = (event: any) => {
+    event.preventDefault();
+    const target = event.target.closest("a");
+    if (!target) return;
+    const url = target.getAttribute("href");
+    if (!url) return;
+
+    PopoutsModule.ShowPopout({
+      id: "html-embed-url-sus",
+      component: "OpenLinkConfirm",
+      data: { url }
+    });
+  };
+
+  const createEl = (json: JsonInput, children: any[]) =>
+    h(
+      json.tag,
+      {
+        style: json.styles,
+        ...json.attributes
+      },
+      children
+    );
+
+  const template = (json: JsonInput) => {
+    if (json.attributes?.href) {
+      if (!json.attributes.href.startsWith("http")) {
+        json.attributes.href = "https://" + json.attributes.href;
+      }
+    }
+    if (json.styles?.backgroundImage) {
+      json.styles.backgroundImage = `url(${
+        process.env.VUE_APP_IMAGE_PROXY_URL
+      }${encodeURIComponent(json.styles.backgroundImage)})`;
+    }
+    if (json.tag === "img" && json.attributes?.src) {
+      json.attributes.src =
+        process.env.VUE_APP_IMAGE_PROXY_URL +
+        encodeURIComponent(json.attributes.src);
+    }
+    if (json.styles?.position === "fixed") {
+      delete json.styles.position;
+    }
+    if (json.styles?.background) {
+      delete json.styles.background;
+    }
+
+    let children: any[] = [];
+    if (typeof json.content === "object") {
+      if (Array.isArray(json.content)) {
+        for (let i = 0; i < json.content.length; i++) {
+          children.push(template(json.content[i]));
+        }
+        return createEl(json, children);
+      } else {
+        children.push(template(json.content));
+        return createEl(json, children);
+      }
+    }
+    if (json.content) {
+      children = [
+        <div class="content">
+          {h(Markup, {
+            text: json.content
+          })}
+        </div>
+      ];
+    }
+    return createEl(json, children);
+  };
+
+  return h("div", { class: "html-embed", on: { click: clickEvent } }, [
+    template(obj)
+  ]);
+}
 </script>
 
 <style scoped>
