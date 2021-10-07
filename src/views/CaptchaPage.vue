8c52aef2530d04490d86d95857d4aff8d63ea6af
Supertiger <saawalkhn@googlemail.com>
2021-10-07 22:04:18 +0100

    added captcha page for mobile

---
+++ src/views/CaptchaPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <Captcha />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Captcha from "@/components/Captcha.vue";
+export default defineComponent({
+  components: { Captcha },
+});
+</script>
